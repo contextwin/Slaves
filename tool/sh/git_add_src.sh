b293652b6ee5d29933bb5167264fba898585bb8e
contextwin <contextwin@example.com>
2020-02-17 12:27:17 +0900

    hoge

--- tool/sh/git_add_src.sh
+++ tool/sh/git_add_src.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/bin/git add main.c function.h view.h mysdl2.h specific.h tool image fonts doc README Makefile Slaves-0.1
+/usr/bin/git add main.c function.h view.h mysdl2.h specific.h tool image fonts doc music README Makefile Slaves-0.1
 /usr/bin/git commit -m "hoge"
 /usr/bin/git push origin master
