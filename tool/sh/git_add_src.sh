b78aa3efe52c8af07595d04675427b38d34b3b65
contextwin <contextwin@example.com>
2020-02-18 20:18:47 +0900

    2020年  2月 18日 火曜日 20:18:47 JST

--- tool/sh/git_add_src.sh
+++ tool/sh/git_add_src.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+DATE=`date`
 /usr/bin/git add main.c function.h view.h mysdl2.h specific.h tool image fonts doc music README Makefile Slaves-0.1
-/usr/bin/git commit -m "hoge"
+/usr/bin/git commit -m "${DATE}"
 /usr/bin/git push origin master
