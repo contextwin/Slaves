81955a6fbab5ab44e7e09b047c4442ef6fea1229
contextwin <contextwin@example.com>
2020-03-29 05:00:16 +0900

    2020年  3月 29日 日曜日 05:00:16 JST

---
+++ tool/sh/wgetslavesdoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+DATE=`date "+%Y%m%d_%H%M%S"`
+THISPROJECTS="/home/contextwin/work/src/Slaves/Slaves/doc/thisproject/"
+wget -O "${THISPROJECTS}5307375/${DATE}.html" http://contextwin.livedoor.blog/archives/5307375.html
+wget -O "${THISPROJECTS}5331764/${DATE}.html" http://contextwin.livedoor.blog/archives/5331764.html
+wget -O "${THISPROJECTS}5402599/${DATE}.html" http://contextwin.livedoor.blog/archives/5402599.html
+wget -O "${THISPROJECTS}5422771/${DATE}.html" http://contextwin.livedoor.blog/archives/5422771.html
+wget -O "${THISPROJECTS}5435893/${DATE}.html" http://contextwin.livedoor.blog/archives/5435893.html
