de38e35745c98f204c5bfb71da287c81f47d831e
contextwin <contextwin@example.com>
2020-03-31 16:16:35 +0900

    2020年  3月 31日 火曜日 16:16:35 JST

--- tool/sh/wgetslavesdoc.sh
+++ tool/sh/wgetslavesdoc.sh
@@ -4,11 +4,6 @@ THISPROJECTS="/home/contextwin/work/src/Slaves/Slaves/doc/thisproject/"
 for LIST in "5307375" "5331764" "5402599" "5422771" "5435893"
 do
 wget -O "${THISPROJECTS}${LIST}/${DATE}.html" http://contextwin.livedoor.blog/archives/${LIST}.html
-#wget -O "${THISPROJECTS}5307375/${DATE}.html" http://contextwin.livedoor.blog/archives/5307375.html
-#wget -O "${THISPROJECTS}5331764/${DATE}.html" http://contextwin.livedoor.blog/archives/5331764.html
-#wget -O "${THISPROJECTS}5402599/${DATE}.html" http://contextwin.livedoor.blog/archives/5402599.html
-#wget -O "${THISPROJECTS}5422771/${DATE}.html" http://contextwin.livedoor.blog/archives/5422771.html
-#wget -O "${THISPROJECTS}5435893/${DATE}.html" http://contextwin.livedoor.blog/archives/5435893.html
 done
 
 for LIST in "5307375" "5331764" "5402599" "5422771" "5435893"
