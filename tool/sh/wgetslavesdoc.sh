898b0c6ce2dfabb8346bbdbce71249900aaff66a
contextwin <contextwin@example.com>
2020-03-31 17:35:04 +0900

    2020年  3月 31日 火曜日 17:35:04 JST

--- tool/sh/wgetslavesdoc.sh
+++ tool/sh/wgetslavesdoc.sh
@@ -5,14 +5,22 @@ THISPROJECTS="/home/contextwin/work/src/Slaves/Slaves/doc/thisproject/"
 
 wget http://contextwin.livedoor.blog
 cat index.html | grep sidebody | grep -v 2020 | awk '{print substr($0, index($0, "archives/"))}' | awk '{sub(".html.*", "");print $0;}' | awk '{sub("archives/", "");print $0;}' > page_list.txt
+
 rm index.html
 
 for LIST in `cat page_list.txt`
 do
-wget -O "${THISPROJECTS}${LIST}/${DATE}.html" http://contextwin.livedoor.blog/archives/${LIST}.html
+ if [ ! -e ${THISPROJECTS}${LIST} ]; then
+ mkdir ${THISPROJECTS}${LIST}
+ fi
 done
 
-for LIST in "5307375" "5331764" "5402599" "5422771" "5435893"
+for LIST in `cat page_list.txt`
+do
+ wget -O "${THISPROJECTS}${LIST}/${DATE}.html" http://contextwin.livedoor.blog/archives/${LIST}.html
+done
+
+for LIST in `cat page_list.txt`
 do
 WC=`diff ${THISPROJECTS}${LIST}/${DATE}.html ${THISPROJECTS}${LIST}/curent/*.html | wc -l`
 if [ ${WC} -gt 0 ]; then
