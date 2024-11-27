e73f1f7b8bea5f49feb86a40973f3b243cceda98
contextwin <contextwin@gmail.com>
2024-11-27 23:53:19 +0900

    ファイル分割

---
+++ image.c
@@ -0,0 +1,21 @@
+#include <Imlib2.h>
+
+/* 画像を描画する */
+void MyDrawImage( char *imagepath, int x, int y, int width, int height ) {
+ 
+ Imlib_Image buffer, image;
+ int w, h;
+
+ buffer = imlib_create_image( width, height );
+ image = imlib_load_image( imagepath );
+ imlib_context_set_image(image);
+ w = imlib_image_get_width();
+ h = imlib_image_get_height();
+ imlib_context_set_image( buffer );
+ imlib_blend_image_onto_image( image, 0, 
+                                         0, 0, w, h, 
+                                          0, 0, width, height );
+ imlib_render_image_on_drawable( x, y );
+ imlib_free_image();
+
+}
---
+++ image.h
@@ -0,0 +1,2 @@
+/* 画像を描画する */
+void MyDrawImage( char *imagepath, int x, int y, int width, int height );
