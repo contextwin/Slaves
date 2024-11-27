e9c6b01437354032c619a036f0b1485b036f389c
contextwin <contextwin@gmail.com>
2024-11-27 23:54:54 +0900

    コメントアウト削除

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <Imlib2.h>
 
+#include "image.h"
+
 /* xbmデータのインクルード */
 #include "./image/current/menue1/test.xbm"
 
@@ -25,26 +27,6 @@
 /* ウィンドウサイズを計算する関数 */
 int MyWindowsize( int n ) {
  return MyROOT_NUM + ((n - 1) * (MyROOT_NUM + MyPADDING));
-}
-
-/* 画像を描画する */
-void MyDrawImage( char *imagepath, int x, int y, int width, int height ) {
- 
- Imlib_Image buffer, image;
- int w, h;
-
- buffer = imlib_create_image( width, height );
- image = imlib_load_image( imagepath );
- imlib_context_set_image(image);
- w = imlib_image_get_width();
- h = imlib_image_get_height();
- imlib_context_set_image( buffer );
- imlib_blend_image_onto_image( image, 0, 
-                                         0, 0, w, h, 
-                                          0, 0, width, height );
- imlib_render_image_on_drawable( x, y );
- imlib_free_image();
-
 }
 
 int main(void)
